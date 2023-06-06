0bde8d4fa17ae0c8f0aa18c67104cf5eb72bf160
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-06-06 09:35:53 -0700

    adv 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
-    c = i + ord('A')
+    x = i + ord('A')
     if i % 2 == 1:
-        c += 32
-    print("{:x}".format(c), end="")
+        x += 32
+    print("{:c}".format(x), end="")
