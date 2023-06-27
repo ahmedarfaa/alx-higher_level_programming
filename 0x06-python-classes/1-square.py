384053fb4780adb93fb9147ce5262e6ffffb91d7
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-06-27 04:37:42 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize new Square.
+        Args:
+            size (int): size of new square.
+        """
+        self.__size = size
