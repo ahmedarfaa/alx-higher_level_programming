1893057fa7902974321552e6c37e0afc223c416f
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-07-11 19:14:44 +0300

    pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/pyhton3
-""" Defines afunction  that write to afile"""
+""" Defines a write_file-function  that write to afile"""
 
 
 def write_file(filename="", text=""):
-    """ writeing to afile and create it if it doesnot exits"""
+    """ writeing to afile and create it if it doesnot exits
+    args:
+        filename: ..
+        text:..
+        """
     with open(filename, "w", encoding="utf-8") as f:
         return(f.write(text))
