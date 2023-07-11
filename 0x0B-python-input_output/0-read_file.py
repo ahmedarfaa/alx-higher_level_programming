951fc59a62127991c9eed6883e8ca37e23e430a9
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-07-11 08:42:58 -0700

    dir created task 0 added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
-#!/usr/bin/pyhton3
-"""Define afunction that reads afile"""
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """Read_file function that prints the content to stdout"""
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
