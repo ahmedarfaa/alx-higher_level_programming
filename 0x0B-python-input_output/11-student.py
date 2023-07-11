6c4ef518d8bcc0c878b90f0fa5f643fca5170ddb
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-07-11 21:31:50 +0300

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines aclass Student"""
+
+
+class Student:
+    """ Student class"""
+    def __init__(self, first_name, last_name, age):
+        """ Initalizing the attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the class of all attributes"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """" Reload function"""
+        for x, y in json.items():
+            setattr(self, x, y)
