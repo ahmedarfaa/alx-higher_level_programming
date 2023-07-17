71e09c0fa179dd9e607ff1dbfb388f696361eb92
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-07-17 13:00:47 +0300

    add_dict method for rec & square added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -128,3 +128,9 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Defining anew method """
+        obj_dictionary = {'id': self.id, 'height': self.height,
+                          'width': self.width, 'x': self.x, 'y': self.y}
+        return (obj_dictionary)
